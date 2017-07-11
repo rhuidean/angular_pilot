88d79efbbad9d5d2e06ea501d9da58e809009a74
Jason <rhuidean06@gmail.com>
2017-07-11 15:24:44 -0500

    add app component

--- myFirstAngularApp/src/app/app.component.ts
+++ myFirstAngularApp/src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// defined variable inside the class can be printed
 export class AppComponent {
   title = 'app';
+  x: number = 7;
+  y: number = 9;
+  mystr: string="WOW";
+  myBoolean= true;
+  myArray = [1,2,3,4,5];
+  user ={
+  	firstName:'Darth',
+  	lastName: 'Vader',
+  }
 }
